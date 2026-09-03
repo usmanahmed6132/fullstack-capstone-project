36c9fbb71a1a3db4f98933cadee447db4096ddac
usman <usmanahmed6132@gmail.com>
2026-09-03 11:48:38 +0500

    Add registration API call in RegisterPage.js

---
+++ RegisterPage.js
@@ -0,0 +1,7 @@
+const response = await fetch('/api/auth/register', {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  body: JSON.stringify({ email, password }),
+});
